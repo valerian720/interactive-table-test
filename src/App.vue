7a97eadc1efac3d5aeca6688fea9a844e21ee246
Валериан Воротников <valerian7202@gmail.com>
2023-05-18 19:10:02 +0300

    basic frame

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+    <router-link class="navbar-brand col-sm-3 col-md-2 mr-0" to="/"
+      >ООО Рога и копыта</router-link
+    >
+    <input
+      class="form-control form-control-dark w-100 m-1 p-1"
+      type="text"
+      placeholder="Поиск"
+      aria-label="Поиск"
+    />
+    <ul class="navbar-nav px-3">
+      <li class="nav-item text-nowrap">
+        <a class="nav-link" href="#">Войти</a>
+      </li>
+    </ul>
   </nav>
   <router-view />
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="less"></style>
